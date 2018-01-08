9813a347d3b18b4962ea8a304ede7e0b172cbac4
Sixian Lin <davidlin006811@gmail.com>
2018-01-08 14:52:00 -0500

    Add files via upload

---
+++ CST8257Lab7/Lab7Scripts/Site.js
@@ -0,0 +1,9 @@
+function ChangeSemester(){
+    var link = "CourseSelection.php?selectedSemesterCode=" + $('#selectedSemester').val();
+     window.location.replace(link);
+} 
+function confirmDelete(){
+     if (!confirm("Selected registrations will be deleted!")) {
+                    return false;
+        }
+}
